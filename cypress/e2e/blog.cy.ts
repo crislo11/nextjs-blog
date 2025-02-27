ba2001671f7795699675ca66e7b02c3c086c07ca
Cristian Lopez <cristian-894@hotmail.com>
2025-02-26 19:56:52 -0500

    refactor: clean up e2e test

--- cypress/e2e/blog.cy.ts
+++ cypress/e2e/blog.cy.ts
@@ -1,15 +1,12 @@
 describe("Blog Post Page", () => {
   it("displays the post and comments", () => {
-    // Visit the blog post page
     cy.visit("http://localhost:3000/post-1");
 
-    // Verify the post title and content
     cy.contains(
       "sunt aut facere repellat provident occaecati excepturi optio reprehenderit"
     ).should("exist");
     cy.contains("quia et suscipit suscipit ").should("exist");
 
-    // Verify the comments section
     cy.contains("Comments").should("exist");
     cy.contains("id labore ex et quam laborum").should("exist");
     cy.contains(
@@ -18,20 +15,16 @@ describe("Blog Post Page", () => {
   });
 
   it("allows adding a new comment", () => {
-    // Visit the blog post page
     cy.visit("http://localhost:3000/post-1");
 
-    // Fill out the comment form
     cy.get('input[placeholder="Your name"]').type("Jane Doe");
     cy.get('input[placeholder="Your email"]').type("jane@example.com");
     cy.get('textarea[placeholder="Your comment"]').type(
       "This is a new comment."
     );
 
-    // Submit the form
     cy.contains("Add Comment").click();
 
-    // Verify the new comment is displayed
     cy.contains("Jane Doe").should("exist");
     cy.contains("This is a new comment.").should("exist");
   });
